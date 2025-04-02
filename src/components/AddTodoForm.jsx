9143f56ed339e744f92773b184bbfd7bcae4fe14
Srinila-04 <srinila042005@gmail.com>
2025-04-02 10:47:39 +0530

    Updated card component size

--- src/components/AddTodoForm.jsx
+++ src/components/AddTodoForm.jsx
@@ -19,7 +19,7 @@ function AddTodoForm(props)
         <div className="flex flex-col gap-3">
             <h1 className="text-2xl font-medium">Manage Activities</h1>
             <div>
-                <input value={newactivity} onChange={handleChange} type="text" className="border border-black bg-transparent p-1" placeholder="Next-activity?"/>
+                <input value={newactivity} onChange={handleChange} type="text" className="border border-black bg-transparent p-1 w-48" placeholder="Next-activity?"/>
                 <button onClick={addActivity} className="bg-black text-white p-1 border border-black">Add</button>
             </div>   
         </div>
